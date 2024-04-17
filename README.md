486c6d50dcd53f42579af2c67ca4191e475c54c1
sylvie98 <sylviemasengesho5@gmail.com>
2024-04-17 23:03:05 +0200

    to focus on about and footer

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # ReactPersonalPortifio
 react resonal portfio cantain five section like home ,about me, project,skilland contact.
-this project deplyed on render
+this project deployed on render
 # https://reactpersonalportifio-1.onrender.com/
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Navigation from './component/Navigation'
+import Footer from './component/Footer'
+import About from './component/About'
 const App = () => {
   return (
     <>
     <Navigation/>
+    <About/>
+    <Footer/>
     </>
   )
 }
